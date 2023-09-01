0098836c299fafdc312bd256b453ce2e9279ea8d
Cr-Israel <carlosisrael08@hotmail.com>
2023-09-01 12:49:47 -0300

    feat: modifications

--- src/core/entities/unique-entity-id.ts
+++ src/core/entities/unique-entity-id.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'node:crypto'
 
 export class UniqueEntityID {
-  private readonly value: string
+  private value: string
 
   toString () {
     return this.value
--- src/domain/application/user/repositories/users-repository.ts
+++ src/domain/application/user/repositories/users-repository.ts
@@ -3,7 +3,7 @@ import { type User } from '../../../entities/user'
 export abstract class UsersRepository {
   abstract findByEmail (email: string): Promise<User | null>
   abstract findAll (): Promise<User[]>
-  abstract update (user: User): Promise<void>
-  abstract remove (user: User): Promise<void>
+  abstract update (id: User['_id']): Promise<void>
+  abstract remove (id: User['_id']): Promise<void>
   abstract create (user: User): Promise<void>
 }
