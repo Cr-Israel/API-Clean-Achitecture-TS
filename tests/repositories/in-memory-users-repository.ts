35867730cf47d2ca566f1aaa11d3b48f2813c9ba
Cr-Israel <carlosisrael08@hotmail.com>
2023-09-01 12:50:23 -0300

    feat: something

--- tests/repositories/in-memory-users-repository.ts
+++ tests/repositories/in-memory-users-repository.ts
@@ -13,6 +13,34 @@ export class InMemoryUsersRepository {
     return user
   }
 
+  async findAll () {
+    return this.items
+  }
+
+  async update (id: User['_id'], updatedUser: User) {
+    // const userIndex = this.items.findIndex((item) => item.email === updatedUser.email)
+
+    // if (userIndex === -1) {
+    //   throw new Error('User not found.')
+    // }
+
+    // this.items[userIndex].email = updatedUser.email
+    // this.items[userIndex].name = updatedUser.name
+    // this.items[userIndex].password = updatedUser.password
+
+    const userIndex = this.items.findIndex((user) => user.id === id)
+
+    if (userIndex === -1) {
+      throw new Error('User not found.')
+    }
+
+    // Atualiza as informações do usuário com base no índice encontrado
+    this.items[userIndex] = {
+      ...this.items[userIndex], // Mantém as informações existentes do usuário
+      ...updatedUser // Atualiza com as novas informações
+    }
+  }
+
   async create (user: User) {
     this.items.push(user)
   }
