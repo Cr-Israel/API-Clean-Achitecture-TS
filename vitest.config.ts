c01395fe460e75c9b5ea0b92d2054f121cc3a24a
Cr-Israel <carlosisrael08@hotmail.com>
2023-09-01 12:08:09 -0300

    chore: fix some errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vitest/config';
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
-    testTimeout: 100000
+    globals: true,
+    root: './'
   },
-});
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' }
+    })
+  ]
+})
