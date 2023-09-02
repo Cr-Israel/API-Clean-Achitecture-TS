02d9d1dba14c08ca7c8802e41ab134f2b99c4f63
Cr-Israel <carlosisrael08@hotmail.com>
2023-09-02 16:56:40 -0300

    feat: delete method update

--- src/domain/application/user/repositories/users-repository.ts
+++ src/domain/application/user/repositories/users-repository.ts
@@ -3,7 +3,6 @@ import { type User } from '../../../entities/user'
 export abstract class UsersRepository {
   abstract findByEmail (email: string): Promise<User | null>
   abstract findAll (): Promise<User[]>
-  abstract update (id: User['_id']): Promise<void>
-  abstract remove (id: User['_id']): Promise<void>
+  abstract delete (user: User): Promise<void>
   abstract create (user: User): Promise<void>
 }
